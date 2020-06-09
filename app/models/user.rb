d444446bdb3a02041736154b269044f417a7688a
Keiichi Katsuno <keichi031605@gmail.com>
2020-06-09 14:40:22 +1000

    FEAT: fix a conflict in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,7 @@ class User < ApplicationRecord
 
   # Validations
   validates :username, presence: true, uniqueness: true
-<<<<<<< HEAD
-  validates :username, format: { with: /^[a-zA-Z0,9\.\-\_]{3,24}$/, message: "Username must be between 3 and 24 letters long and can contain the letters A-Z, 0-9 , or the characters '.' , '/' and '_' . " }
-=======
   validates :username, format: { with: /\A[a-zA-Z0-9\.\-\_]{3,24}\z/, message: "Username must be between 3 and 24 letters long and can contain the letters A-Z, 0-9 , or the characters '.' , '/' and '_' . " }
->>>>>>> master
   validates :bio, presence: true, length: { maximum: 500 }
 
   def followers
