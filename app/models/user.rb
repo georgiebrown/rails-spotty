2c8afec94042b1a2b2b950c46f70d0569ca94107
Robert Chapman <robchapman94@gmail.com>
2020-06-09 14:18:39 +1000

    Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   # Associations
   has_many :stories
   has_many :favourites
@@ -11,19 +12,15 @@ class User < ApplicationRecord
   has_many :follows
 
   # Validations
-  validates :username, precense: true, uniqueness: true
-  validates :username, format: { with: /^[a-zA-Z0-9\.\-\_]{3,24}$/, message: "Username must be between 3 and 24 letters long and can contain the letters A-Z, 0-9 , or the characters '.' , '/' and '_' . " }
-  validates :bio, precense: true, length: { maximum: 500 }
+  validates :username, presence: true, uniqueness: true
+  validates :username, format: { with: /\A[a-zA-Z0-9\.\-\_]{3,24}\z/, message: "Username must be between 3 and 24 letters long and can contain the letters A-Z, 0-9 , or the characters '.' , '/' and '_' . " }
+  validates :bio, presence: true, length: { maximum: 500 }
 
   def followers
-    follows.map do |follow|
-      follow.follower
-    end
+    follows.where("leader_id = #{id}").map { |f| f.follower }
   end
 
   def following
-    follows.map do |follow|
-      follow.leader
-    end
+    follows.where("follower_id = #{id}").map { |f| f.follower }
   end
 end
