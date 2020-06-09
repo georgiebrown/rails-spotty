ded74a6121ce5e88409f9b3341bb1b6c0ab45cbb
Keiichi Katsuno <keichi031605@gmail.com>
2020-06-09 12:49:05 +1000

    FEAT: Simply implemented spots/stories controller and its show views

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,11 @@
 class Spot < ApplicationRecord
+  has_many :stories, dependent: :destroy
+  belongs_to :user
+  has_one_attached :photo
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :category, presence: true
+  # geocoded_by :location
+  # after_validation :geocode, if: :will_save_change_to_location?
 end
