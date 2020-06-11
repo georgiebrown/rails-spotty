65290e5751f884dad1686b1c5a9623a474c01696
Nesil Ozer <nesilozr@gmail.com>
2020-06-11 15:22:16 +1000

    adding categoris to bucket list

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -2,10 +2,8 @@ class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :spot
 
-<<<<<<< HEAD
 
-=======
   # Validations
   validates :spot, uniqueness: { scope: :user }
->>>>>>> master
+
 end
