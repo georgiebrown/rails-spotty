a21f43b7f66d2f4e0747a790d636e8f005934bfe
Georgie Brown <georginaabrown@gmail.com>
2020-06-11 10:34:40 +1000

    maps

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,40 +1,30 @@
 class SpotsController < ApplicationController
   def index
-
     if params[:query_attributes].present?
       @spots = Spot.geocoded.global_search(params[:query_attributes])
-
     else
       @spots = Spot.geocoded
     end
 
-
     @spots = @spots.near(params[:query_location], 5) if params[:query_location].present? && !@spots.empty?
     # spots.near(location search)
     @markers = @spots.map do |spot| {
-
       lat: spot.latitude,
       lng: spot.longitude,
       infoWindow: { content: render_to_string(partial: "info_window", locals: { spot: spot }) }
       # image_url: helpers.asset_url('icon.png')
     }
     end
+
   end
+
 
   def show
     @story = Story.new
     @spot = Spot.find(params[:id])
     user = current_user
-    if user
+      if user
       @stories = @spot.stories.where("user_id = ")
-    end
-      #   @markers =
-      # [{
-      #   lat: @spot.latitude,
-      #   lng: @spot.longitude,
-      #   infoWindow: render_to_string(partial: "info_window", locals: { spot: @spot }),
-      #   image_url: helpers.asset_url('map_icon.png')
-      # }]
+      end
   end
-
 end
