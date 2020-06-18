a4725aa7042f8f85c03b7b49be288d4cc1ae7775
Keiichi Katsuno <keichi031605@gmail.com>
2020-06-18 15:26:21 +1000

    FEAT: Fixed owl carousel to make heroku work properly

--- app/javascript/components/init_owl_carousel.js
+++ app/javascript/components/init_owl_carousel.js
@@ -1,6 +1,5 @@
 import JQuery from 'jquery';
 window.$ = window.JQuery = JQuery;
-import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 const initOwlCarousel = () => {
   $(document).ready(function () {
